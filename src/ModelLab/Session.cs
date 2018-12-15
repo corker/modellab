a537ccbe6ed28bb8d7cd8809a2ffab1181bbfe40
Michael Borisov <michael.borisov@franx.com>
2018-12-15 21:10:05 +0100

    cleanup

--- src/ModelLab/Session.cs
+++ src/ModelLab/Session.cs
@@ -1,42 +1,15 @@
 ﻿using System;
-using System.Linq;
 using ModelLab.DependencyInjection;
-using ModelLab.Expressions;
 using ModelLab.Sessions;
 
 namespace ModelLab
 {
-    public sealed class Session : IRunSessions
+    public static class Session
     {
-        private readonly IExecuteSessionActions _actions;
-        private readonly IProvideSessionElements _elements;
-        private readonly IProvideSessionSettings _settings;
-
-        public Session(
-            IProvideSessionElements elements,
-            IProvideSessionSettings settings,
-            IExecuteSessionActions actions
-        )
-        {
-            _elements = elements;
-            _settings = settings;
-            _actions = actions;
-        }
-
-        public void Run()
-        {
-            var element = _elements.FindByName(_settings.StartNodeName).FirstOrDefault();
-            while (element != null)
-            {
-                _actions.ExecuteFor(element);
-                element = element.Next();
-            }
-        }
-
         public static void Run(Action<IRegisterServices> configure)
         {
             var builder = new ServiceRegistryBuilder();
-            builder.Use<Session>();
+            builder.Use<SessionRunner>();
             builder.Use<SessionElementProvider>();
             builder.Use<SessionActionExecutor>();
             configure(builder);
---
+++ src/ModelLab/Sessions/SessionRunner.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using ModelLab.Sessions;
+
+namespace ModelLab
+{
+    public class SessionRunner : IRunSessions
+    {
+        private readonly IExecuteSessionActions _actions;
+        private readonly IProvideSessionElements _elements;
+        private readonly IProvideSessionSettings _settings;
+
+        public SessionRunner(
+            IProvideSessionElements elements,
+            IProvideSessionSettings settings,
+            IExecuteSessionActions actions
+        )
+        {
+            _elements = elements;
+            _settings = settings;
+            _actions = actions;
+        }
+
+        public void Run()
+        {
+            var element = _elements.FindByName(_settings.StartNodeName).FirstOrDefault();
+            while (element != null)
+            {
+                _actions.ExecuteFor(element);
+                element = element.Next();
+            }
+        }
+    }
+}
